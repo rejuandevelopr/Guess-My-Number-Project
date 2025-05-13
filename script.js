4a1764456a39a984c8529e3658e432ddac65e020
S.M.Rejuanul Islam <rejuanhossain8@gmail.com>
2025-05-13 22:06:51 +0600

    1st

---
+++ script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+document.querySelector('.message').textContent = 'Correct Number!';
+console.log(document.querySelector('.message').textContent);
+
+
+document.querySelector('.number').textContent  = 13;
+document.querySelector('.score').textContent = 20;
+
+document.querySelector('.guess').value = 23;
+
+console.log(document.querySelector('.guess').value);
